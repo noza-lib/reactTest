29c4f133cc8f3760c14782cf19a8fae806053b3d
noza-lib <souma.nozawa.lib@gmail.com>
2025-09-01 06:29:11 +0900

    3.9

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+
 function Form(props) {
+    const [name, setName] = useState("");
+    function handleChange(event) {
+        setName(event.target.value);
+      }
+    function handleSubmit(event) {
+        event.preventDefault();
+        props.onSubmit(name);
+        setName("");
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
             What needs to be done?
@@ -12,6 +23,8 @@ function Form(props) {
           className="input input__lg"
           name="text"
           autoComplete="off"
+          value={name}
+          onChange={handleChange}
         />
         <button type="submit" className="btn btn__primary btn__lg">
           Add

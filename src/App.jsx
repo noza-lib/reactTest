252fcba4945dc6aa35306232ffdd0e0e09eeac43
noza-lib <souma.nozawa.lib@gmail.com>
2025-09-02 07:31:53 +0900

    4.6

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,16 @@ import FilterButton from "./components/FilterButton";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
   const taskList = tasks?.map((task) => 
   <Todo 
     id={task.id} 
@@ -17,6 +25,14 @@ function App(props) {
     editTask={editTask} 
   />
   );
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
   function addTask(name) {
@@ -50,9 +66,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form onSubmit={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
